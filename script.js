87d56e64b9f84b3244b6f217a8adceb943ee3c36
Cellsito <celsobfigueroa@gmail.com>
2024-05-16 17:20:20 -0300

    Feat: funções java não atualizadas

--- script.js
+++ script.js
@@ -27,3 +27,116 @@ onresize = () => {
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
 }
+
+// Função Carrosel
+
+let slides = [
+    'primeiro-banner',
+    "segundo-banner",
+    'terceiro-banner'
+]
+
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    // Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    // numeroSlides = 3
+    // numeroSlides - 1 -> 2
+    // estou no ultimo? 2
+
+    // [0, 1, 2]
+
+    if (slideAtual < (numeroSlides - 1)) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarSlideAnterior = () => {
+    // Remove slide anterior
+
+    // numeroSlides = 3
+    // numeroSlides - 1 -> 2
+
+    // [0, 1, 2]
+
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+        slideAtual = numeroSlides - 1
+    }
+
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+const selecionarSlide = (indiceSlide) => {
+    slides.forEach(slide => banner.classList.remove(slide))
+
+    slideAtual = indiceSlide
+
+    banner.classList.add(slides[indiceSlide])
+}
+
+let listaCases = []
+
+const renderizarCases = () => {
+    let elementoLista = document.getElementById("lista-cards")
+
+    // Template Strings
+    let template = ""
+
+    listaCases.forEach( cardCase => {
+        template += `<div class="card">
+            <img src="${cardCase.imagem}" alt="">
+            <p>${cardCase.descricao}</p>
+            <button>Ver mais</button>
+        </div>` 
+    })
+
+    elementoLista.innerHTML = template
+}
+
+const carregarCases = () => {
+    fetch("http://localhost:3000/cases")
+    .then( (resposta) => resposta.json() )
+    .then( (dados) => {
+        listaCases = dados
+        renderizarCases()
+    })
+    .catch( erro => console.error(erro))
+}
+
+
+const solicitarOrcamento = () => {
+    // Pegar valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    console.log(valorNome);
+    console.log(valorEmail);
+    console.log(valorDescricao);
+
+    // Organizar objeto com os valores
+    // Enviar requisicao para a api
+        // Limpar os campos
+        // Mostrar alert com msg de sucesso
+        // CASO ERRO - alert com msg erro
+}
